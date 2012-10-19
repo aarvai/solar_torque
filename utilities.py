f508f3c66a646cf14d5b9a145abb4512fced5c5b
aarvai <aarvai@cfa.harvard.edu>
2012-10-19 13:23:02 -0400

    Minor formatting changes
    Enahanced a few comments

--- utilities.py
+++ utilities.py
@@ -7,12 +7,12 @@ from bad_times import nsm, ssm
 def overlap(table1, table2):
     # This function looks for overlaps between two sets of ranges 
     # (usually times, formatted in seconds). 
-    # It will output an array equal in length to table1.
+    # It will output a boolean array equal in length to table1.
     # Each input table must be formatted with two columns: 
     # range start and range stop.
-    # The output will be 1 if the corresponding range in table1
+    # The output will be True if the corresponding range in table1
     # overlaps with any range in table2.  The output will be
-    # otherwise zero.
+    # otherwise False.
     out = np.zeros(np.size(table1, axis=0), dtype='bool')
     for i in range(np.size(table1, axis=0)):
         s1_s2 = table1[i, 0] < table2[:, 0] 
@@ -26,9 +26,9 @@ def overlap(table1, table2):
     return out
 
 def str_to_secs(table):
-    # This function will take a table of time ranges
-    # formatted as strings (compatible with bad_times)
-    # and convert it to a nx2 array in DateTime seconds
+# This function will take a table of time ranges
+# formatted as strings (compatible with bad_times)
+# and convert it to a nx2 array in DateTime seconds
     out = np.zeros([len(table), 2])
     for i in range(len(table)):
         t1, t2 = table[i].split()
