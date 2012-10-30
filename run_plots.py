b1bb99f2352fdcf98d276b3f61f8b7dc0f21c976
aarvai <aarvai@cfa.harvard.edu>
2012-10-30 13:09:06 -0400

    Added 3D surface plots for the old and new torques
    Helps visualize differences

--- run_plots.py
+++ run_plots.py
@@ -1,20 +1,44 @@
-# Plot old solar torques 
-fig = figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(R, P, X_old, cmap='jet')
+# Plot solar torques 
+zipvals = zip((X_old, Y_old, Z_old, X_new, Y_new, Z_new),
+              ('Old X', 'Old Y', 'Old Z', 'New X', 'New Y', 'New Z'))
+for var, var_str in zipvals:
+    fig = figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(R, P, var, cmap='jet')
+    title(var_str + ' Torques [ft-lbf]')
+    xlabel('Roll [deg]')
+    ylabel('Pitch [deg]')
 
+# Compare old and new torques at zero roll angle
+figure()
+zipvals = zip(((X_old, X_new), (Y_old, Y_new), (Z_old, Z_new)),
+              ('X', 'Y', 'Z'),
+              (1,2,3), 
+              ([-0.00006, 0.00008], [-0.0008, 0.0004], [-0.0001, 0.0001]))
+for vars, var_str, sub, ylims in zipvals:
+    subplot(3,1,sub)
+    i = (roll == 0)
+    scatter(pitch[i], torque[i, sub - 1],  c='k', s=2, lw=0)
+    plot(P[30,:], vars[0][30,:], 'b', label='old', lw=2)
+    plot(P[30,:], vars[1][30,:], 'r', label='new', lw=2)
+    title(var_str + ' Torques for Zero Roll Angle')
+    legend(loc='best')
+    grid()
+    xlabel('Pitch Angle [deg]')
+    ylabel(var_str + ' Torque [ft-lbf]')
+    ylim(ylims)
+tight_layout()
+    
 # Plot torques by time (to identify outliers for filtering)
 print('plotting...')
 for i in range(3):
-    figure(i+1)
+    figure()
     plot_cxctime(t1 + 1/2 * dur, torque[:,i], 'b*')
 
 # Plot torques by time, colored by roll, pitch, and dur (again for outliers)
-zipvals = zip((4, 5, 6), 
-              (roll_1, pitch_1, dur), 
-              ('Roll', 'Pitch', 'Duration'))
-for fig, var, var_str in zipvals:
-    figure(fig)
+zipvals = zip((roll_1, pitch_1, dur), ('Roll', 'Pitch', 'Duration'))
+for var, var_str in zipvals:
+    figure()
     
     subplot(3,1,1)
     title('Color = ' + var_str)
@@ -33,7 +57,7 @@ for fig, var, var_str in zipvals:
     colorbar()
 
 # Plot torques by attitude (to see the raw "averaged" data w/o surface fit)
-figure(7)
+figure()
 subplot(3, 1, 1)
 scatter(avg_pitch, avg_roll, c=avg_torque[:,0], marker='o', cmap=cm.jet, lw=0)
 colorbar()
@@ -44,7 +68,7 @@ subplot(3 ,1, 3)
 scatter(avg_pitch, avg_roll, c=avg_torque[:,2], marker='o', cmap=cm.jet, lw=0)
 colorbar()
 
-figure(8)
+figure()
 subplot(3, 1, 1)
 scatter(avg_pitch, avg_roll, c=num_dwells, marker='o', cmap=cm.jet, lw=0)
 colorbar()
@@ -56,7 +80,7 @@ subplot(3 ,1, 3)
 scatter(avg_pitch, avg_roll, c=num_dwells, marker='o', cmap=cm.jet, lw=0)
 colorbar()
 
-figure(9)
+figure()
 i = dur < 5000
 subplot(3, 1, 1)
 scatter(t1[i] + 1/2 * dur[i], torque[i,0], c=dur[i], lw=0)
