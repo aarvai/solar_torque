29e111b0685e1106c2e8d548b5fcab6f5700acad
aarvai <aarvai@cfa.harvard.edu>
2012-10-31 12:31:56 -0400

    Cleanned up plotting code, added histograms of error predictions
    Easier to read

--- run_plots.py
+++ run_plots.py
@@ -1,4 +1,4 @@
-# Plot solar torques 
+# Plot solar torques  in 3D
 zipvals = zip((X_old, Y_old, Z_old, X_new, Y_new, Z_new),
               ('Old X', 'Old Y', 'Old Z', 'New X', 'New Y', 'New Z'))
 for var, var_str in zipvals:
@@ -31,9 +31,11 @@ tight_layout()
     
 # Plot torques by time (to identify outliers for filtering)
 print('plotting...')
+labels = ('X', 'Y', 'Z')
 for i in range(3):
     figure()
     plot_cxctime(t1 + 1/2 * dur, torque[:,i], 'b*')
+    title(labels[i] + ' Torque [ft-lbf]')
 
 # Plot torques by time, colored by roll, pitch, and dur (again for outliers)
 zipvals = zip((roll_1, pitch_1, dur), ('Roll', 'Pitch', 'Duration'))
@@ -58,28 +60,21 @@ for var, var_str in zipvals:
 
 # Plot torques by attitude (to see the raw "averaged" data w/o surface fit)
 figure()
-subplot(3, 1, 1)
-scatter(avg_pitch, avg_roll, c=avg_torque[:,0], marker='o', cmap=cm.jet, lw=0)
-colorbar()
-subplot(3, 1, 2)
-scatter(avg_pitch, avg_roll, c=avg_torque[:,1], marker='o', cmap=cm.jet, lw=0)
-colorbar()
-subplot(3 ,1, 3)
-scatter(avg_pitch, avg_roll, c=avg_torque[:,2], marker='o', cmap=cm.jet, lw=0)
-colorbar()
-
+for i in range(3):
+   subplot(3, 1, i + 1)
+   scatter(avg_pitch, avg_roll, c=avg_torque[:,i], marker='o', cmap=cm.jet, lw=0)
+   colorbar()
+   
+# Plot Number of Dwells by attitude
 figure()
 subplot(3, 1, 1)
-scatter(avg_pitch, avg_roll, c=num_dwells, marker='o', cmap=cm.jet, lw=0)
-colorbar()
-title('Color = Number of Dwells')
-subplot(3, 1, 2)
-scatter(avg_pitch, avg_roll, c=num_dwells, marker='o', cmap=cm.jet, lw=0)
-colorbar()
-subplot(3 ,1, 3)
-scatter(avg_pitch, avg_roll, c=num_dwells, marker='o', cmap=cm.jet, lw=0)
-colorbar()
-
+title('Color = Number of Dwells')   
+for i in range(3):
+   subplot(3, 1, i + 1)
+   scatter(avg_pitch, avg_roll, c=num_dwells, marker='o', cmap=cm.jet, lw=0)
+   colorbar()
+   
+# Plot Duration by attitude, capping duration at 5 ksec
 figure()
 i = dur < 5000
 subplot(3, 1, 1)
@@ -95,3 +90,18 @@ subplot(3, 1, 3)
 scatter(t1[i] + 1/2 * dur[i], torque[i,2], c=dur[i], lw=0)
 ylim([-0.0001, 0.0001])
 colorbar()
+
+# Plot histograms of prediction errors 
+pred, act = read_MCC_results('orig_mom_pred_vs_tlm.txt')
+errs = act - pred
+figure()
+labels = ('Roll', 'Pitch', 'Yaw')
+max_error = ceil(abs(np.max(errs)))
+for i in range(3):
+    subplot(3, 1, i + 1)
+    hist(errs[:, i], bins=arange(-max_error, max_error + 1))
+    title(labels[i] + ' Momentum Prediction Errors')
+    xlabel('ft-lbf-sec')
+    ylabel('Occurrences')
+    xlim((-max_error, max_error))
+tight_layout()    
