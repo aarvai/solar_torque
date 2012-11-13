390de05f19a1353e5f76572592d60ab248948599
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2012-11-13 10:24:41 -0500

    Cleaned up plots
    Removed colorbars that didn't work, etc.

--- run_plots.py
+++ run_plots.py
@@ -11,12 +11,16 @@ for var, var_str, col in zipvals:
     fig = figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(R, P, var, cmap='jet')
-    ax.scatter(avg_roll, avg_pitch, avg_torque[:,col])
     title(var_str + ' Torques [ft-lbf]')
     xlabel('Roll [deg]')
     ylabel('Pitch [deg]')
+    ax.set_zlim((np.min(avg_torque[:,col]), np.max(avg_torque[:,col])))
     figname = var_str[:3] + '_' + var_str[4] + '_3d.png'
     savefig(figname.lower())    
+    ax.scatter(avg_roll, avg_pitch, avg_torque[:,col])
+    ax.set_zlim((np.min(avg_torque[:,col]), np.max(avg_torque[:,col])))
+    figname2 = var_str[:3] + '_' + var_str[4] + '_3d_pts.png'
+    savefig(figname2.lower())
 
 # Plot new torques by pitch for various roll angles
 figure()
@@ -53,53 +57,35 @@ for vars, var_str, sub, ylims in zipvals:
     subplot(3,1,sub)
     for pitch_i, plot_str in zipvals2:
         i = (P[0,:] == pitch_i)
-        plot(R[:,i][0], vars[:,i][0], plot_str, label='Pitch = ' + str(pitch_i))
+        plot(R[:,i], vars[:,i], plot_str, label='Pitch = ' + str(pitch_i))
     plot(avg_roll, avg_torque[:, sub - 1], '.')
     title('New ' + var_str + ' Torques for Various Pitch Angles')
-    legend(loc='best')
+    #legend(loc='best')
     grid()
     xlabel('Roll Angle [deg]')
     ylabel(var_str + ' Torque [ft-lbf]')
-    ylim(ylims)
+    #ylim(ylims)
 tight_layout()
 savefig('new_by_roll_various_pitches.png')
 
 # Compare old and new torques at zero roll angle
-figure()
 zipvals = zip(((X_old, X_new), (Y_old, Y_new), (Z_old, Z_new)),
               ('X', 'Y', 'Z'),
-              (1,2,3), 
-              ([-0.00006, 0.00008], [-0.0008, 0.0004], [-0.0001, 0.0001]))
+              (0, 1, 2),
+              ([-0.00003, 0.00004], [-0.00012, 0.0000], [-0.00005, 0.00005]))
+i = (roll == 0)
 for vars, var_str, sub, ylims in zipvals:
-    subplot(3,1,sub)
-    i = (roll == 0)
-    scatter(pitch[i], torque[i, sub - 1],  c='k', s=2, lw=0)
-    plot(P[30,:], vars[0][30,:], 'b', label='old', lw=2)
-    plot(P[30,:], vars[1][30,:], 'r', label='new', lw=2)
+    figure()
+    scatter(pitch[i], torque[i, sub],  c='k', s=2, lw=0)
+    plot(P[30,:], vars[0][30,:], 'b', label='old', lw=5)
+    plot(P[30,:], vars[1][30,:], 'r', label='new', lw=5)
     title(var_str + ' Torques for Zero Roll Angle')
     legend(loc='best')
     grid()
     xlabel('Pitch Angle [deg]')
     ylabel(var_str + ' Torque [ft-lbf]')
     ylim(ylims)
-tight_layout()
-savefig('both_at_0_roll.png')
-
-# Compare old and new torques at zero roll angle  - just pitch torque
-figure()
-i = (roll == 0)
-scatter(pitch[i], torque[i, 1],  c='k', s=2, lw=0)
-plot(P[30,:], Y_old[30,:], 'b', label='old', lw=5)
-plot(P[30,:], Y_new[30,:], 'r', label='new', lw=5)
-title('Y Torques for Zero Roll Angle')
-legend(loc='best')
-grid()
-xlabel('Pitch Angle [deg]')
-ylabel('Y Torque [ft-lbf]')
-ylim([-0.00012, 0.0000])
-tight_layout()
-savefig('both_at_0_roll_pitch_only.png')
-
+    savefig('both_' + var_str.lower() + '_at_0_roll.png')
 
 # Compare old and new torques at 90 deg pitch
 figure()
@@ -141,19 +127,16 @@ for var, var_str in zipvals:
     scatter(t1 + 1/2 * dur, torque[:,0], c=var, lw=0)
     ylabel('X Torque [ft-lbf]')
     ylim([-0.00006, 0.00008])
-    colorbar()
     
     subplot(3, 1, 2)
     scatter(t1 + 1/2 * dur, torque[:,1], c=var, lw=0)
     ylabel('Y Torque [ft-lbf]')
     ylim([-0.0008, 0.0004])
-    colorbar()
     
     subplot(3, 1, 3)
     scatter(t1 + 1/2 * dur, torque[:,2], c=var, lw=0)
     ylabel('Z Torque [ft-lbf]')
     ylim([-0.0001, 0.0001])
-    colorbar()
     
     tight_layout()
     savefig('all_pts_by_time_' + var_str.lower() + '.png')
@@ -162,52 +145,54 @@ for var, var_str in zipvals:
 figure()
 labels = ('X', 'Y', 'Z')
 subplot(3, 1, 1)
-title('Torques by Attitude w/o Surface Fit')
+title('Torques by Attitude w/o Surface Fit, Color = cTorque')
 for i in range(3):
    subplot(3, 1, i + 1)
    scatter(avg_pitch, avg_roll, c=avg_torque[:,i], marker='o', cmap=cm.jet, lw=0)
-   c = colorbar()
-   c.set_label(labels[i] + ' Torque')
+   #c = colorbar()
+   #c.set_label(labels[i] + ' Torque')
    xlabel('Pitch [deg]')
    ylabel('Roll [deg]')
    draw()
+tight_layout()
 savefig('avg_by_att_2d.png')
 
 # Plot torques by pitch
 figure()
 labels = ('X', 'Y', 'Z')
+subplot(3, 1, 1)
+title('Torques by Pitch')
 for i in range(3):
    subplot(3, 1, i + 1)
    plot(avg_pitch, avg_torque[:,i], '.')
    xlabel('Pitch [deg]')
    ylabel(labels[i] + ' Torque [ft-lbf]')
-subplot(3, 1, 1)
-title('Torques by Pitch')
+tight_layout()
 savefig('avg_by_pitch.png')
 
 # Plot torques by roll
 figure()
 labels = ('X', 'Y', 'Z')
+subplot(3, 1, 1)
+title('Torques by Roll')
 for i in range(3):
    subplot(3, 1, i + 1)
    plot(avg_roll, avg_torque[:,i], '.')
    xlabel('Roll [deg]')
    ylabel(labels[i] + ' Torque [ft-lbf]')
-subplot(3, 1, 1)
-title('Torques by Roll')
+tight_layout()
 savefig('avg_by_roll.png')
 
 # Plot torques by attitude - 3D plot (to see the raw "averaged" data w/o surface fit)
-fig = figure()
-zipvals = zip((311, 312, 313), (0, 1, 2))
 labels = ('X', 'Y', 'Z')
-for sub, var in zipvals:
-   ax = fig.add_subplot(sub, projection = '3d')
-   ax.scatter(avg_pitch, avg_roll, avg_torque[:,var])
+for i in range(3):
+   fig = figure()
+   ax = fig.add_subplot(111, projection = '3d')
+   ax.scatter(avg_pitch, avg_roll, avg_torque[:,i])
    xlabel('Pitch [deg]')
    ylabel('Roll[deg]')
-   zlabel(labels[i] + ' Torque [ft-lbf]')
-savefig('avg_by_att_3d.png')
+   title(labels[i] + ' Torque by Att w/o Surface Fit [ft-lbf]')
+   savefig('avg_by_att_' + labels[i].lower() + '_3d.png')
 
 # Plot Number of Dwells by attitude
 figure()
@@ -235,15 +220,15 @@ savefig('avg_by_att_total_dur.png')
 # Plot Duration by time, capping duration at 5 ksec
 figure()
 subplot(3, 1, 1)
-title('Torque by Time, Color = Duration')
+title('Torque by Time, Color = Duration Capped at 5 ksec')
 labels = ('X', 'Y', 'Z')
-i = dur < 5000
-for i in range(3);
+j = dur < 5000
+for i in range(3):
     subplot(3, 1, i + 1)
-    scatter(t1[i] + 1/2 * dur[i], torque[i,0], c=dur[i], lw=0)
-    c = colorbar()
-    c.set_label('Dur [Sec]')
+    scatter(t1[j] + 1/2 * dur[j], torque[j,0], c=dur[j], lw=0)
+    ylim([-.00005, .00005])
     ylabel(labels[i] + ' Torque [ft-lbf]')
+    #c = colorbar()
 tight_layout()
 savefig('avg_by_time_dur.png')
 
@@ -263,15 +248,10 @@ grid()
 title('0 Deg Roll Throughout Time')
 ylabel('Y-Torque [ft-lbf]')
 xlabel('Pitch Angle [deg]')
+tight_layout()
 savefig('new_0_roll_thru_time.png')
 
 chdir('..')    
 #close('all')  
     
     
-    
-
-# Plot just x in 3D
-#fig = figure()
-#ax = fig.add_subplot(111, projection='3d')
-#ax.scatter(avg_pitch, avg_roll, avg_torque[:,0])
