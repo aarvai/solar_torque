4e882609e37a14a169c98355f414848d06acebfd
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2012-11-27 11:43:21 -0500

    Added Z-torque at 90 deg pitch thru time
    Since z-torque isn't a function of roll

--- compare_thru_time.py
+++ compare_thru_time.py
@@ -57,4 +57,23 @@ xlabel('Pitch Angle [deg]')
 tight_layout()
 savefig('new_z_0_roll_thru_time.png')
 
+# Plot Z-Torque at 90 deg Pitch through Time
+f = open('../z_90_pitch_thru_time.txt', 'r')
+lines = f.readlines()
+f.close()
+figure()
+for line in lines[:2]:
+    fields = line.split()
+    plot(arange(-30,31), fields[1:], label=fields[0], linewidth=5)
+for line in lines[2:]:
+    fields = line.split()
+    plot(arange(-30,31), fields[1:], label=fields[0])
+legend(loc='best')
+grid()
+title('90 Deg Pitch Throughout Time')
+ylabel('Z-Torque [ft-lbf]')
+xlabel('Roll Angle [deg]')
+tight_layout()
+savefig('new_z_90_pitch_thru_time.png')
+
 chdir('..')  
